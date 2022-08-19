37199916ba5521a5788641541ac9acb91883f1df
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-08-19 17:47:20 -0400

    feat(tasks): Added a function to get task notes
    - Modified Get-DendronContent to accept filters and patterns for use in
      refining the items returned.
    - Removed 'Mandatory' attribute from path.  It will get the path from
      the current vault if not specified

--- source/PSDendron/public/Get-DendronContent.ps1
+++ source/PSDendron/public/Get-DendronContent.ps1
@@ -7,30 +7,56 @@ function Get-DendronContent {
     [CmdletBinding()]
     param(
         # Specifies a path to one or more locations.
-        [Parameter(Mandatory = $true,
-            Position = 0,
-            ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName = $true
+        [Parameter(
+            Position = 1,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName
         )]
-        [Alias("PSPath")]
-        [ValidateNotNullOrEmpty()]
-        [string[]]$Path
+        [Alias('PSPath')]
+        [string[]]$Path,
+
+        # Filter to apply to filenames
+        [Parameter(
+        )]
+        [string]$Filter,
+
+        # Pattern to use to include files
+        [Parameter(
+        )]
+        [string]$Include,
+
+        # Pattern to use to exclude files
+        [Parameter(
+        )]
+        [string]$Exclude
     )
     begin {
+        Write-Debug "-- Begin $($MyInvocation.MyCommand.Name)"
     }
     process {
+        if (-not($PSBoundParameters.ContainsKey('Path'))) {
+            Write-Verbose "  No path given.  Looking up vault in current directory"
+            $Path = (Get-DendronVault).Path
+        }
         foreach ($p in $Path) {
             try {
                 $item = Get-Item $p -ErrorAction Stop
+                Write-Debug "  Path $($item.Name) given"
                 switch (($item.GetType()).Name) {
                     'FileInfo' {
+                        Write-Debug '  Getting contentinfo for file'
                         if ($item.Extension -eq '.md') {
                             $item | Get-DendronContentInfo | Write-Output
                         }
                         continue
                     }
                     'DirectoryInfo' {
-                        Get-ChildItem $item.FullName | Get-DendronContent
+                        Write-Debug '  Directory given.  Getting content'
+                        Write-Debug "    - Path $($item.FullName)"
+                        Write-Debug "    - Include $Include"
+                        Write-Debug "    - Exclude $Exclude"
+                        Write-Debug "    - Filter  $Filter"
+                        Get-ChildItem -Path $item.FullName @PSBoundParameters | Get-DendronContent
                         continue
                     }
                 }
---
+++ source/PSDendron/public/Get-DendronTaskNote.ps1
@@ -0,0 +1,56 @@
+
+function Get-DendronTaskNote {
+    [CmdletBinding()]
+    param(
+        # Specifies a path to one or more locations.
+        [Parameter(
+            Position = 1,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName
+        )]
+        [Alias('PSPath')]
+        [string[]]$Path,
+
+        # Filter to apply to filenames
+        [Parameter(
+        )]
+        [string]$Filter,
+
+        # Pattern to use to include files
+        [Parameter(
+        )]
+        [string]$Include,
+
+        # Pattern to use to exclude files
+        [Parameter(
+        )]
+        [string]$Exclude
+    )
+    begin {
+        Write-Debug "-- Begin $($MyInvocation.MyCommand.Name)"
+    }
+    process {
+        try {
+            $config = (Get-DendronConfiguration).workspace.task
+            $task_domain = $config.name
+            if (-not([system.string]::IsNullOrEmpty($config.dateFormat))) {
+                $date_path = $config.dateFormat -replace 'y+', '*' -replace 'M+' , '*' -replace 'd+', '*'
+                $task_domain += $date_path
+                $task_domain += '.'
+                Write-Debug "  Adding date to the filter: $task_domain"
+            }
+
+            if (-not($PSBoundParameters.ContainsKey('Filter'))) {
+                $PSBoundParameters['Filter'] = "$($task_domain)*.md"
+            }
+
+            Get-DendronContent @PSBoundParameters | Write-Output
+
+        } catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
+    end {
+        Write-Debug "-- End $($MyInvocation.MyCommand.Name)"
+    }
+}
